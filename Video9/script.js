12809c7794dffbb90b80abaad70009555078b0fd
bethq321 <bethqualls@comcast.net>
2024-12-05 12:01:03 -0600

    attempting to add event listener

---
+++ Video9/script.js
@@ -0,0 +1,42 @@
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 720;
+
+    class InputHandler {
+        constructor(){
+            this.keys = [];
+            window.addEventListener('keydown', function(e){
+                console.log(e);
+            });
+        }
+    }
+
+    class Player {
+
+
+    }
+
+    class Background {
+
+    }
+
+    class Enemy {
+
+    }
+
+    function handleEnemies() {
+
+    }
+
+    function displayStatusText() {
+
+    }
+
+    const input = new InputHandler();
+
+    function animate() {
+
+    }
+});
